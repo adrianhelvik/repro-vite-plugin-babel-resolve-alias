027822384186289676d73194c329603e1bb1590d
Adrian Helvik <adrianhelvik100@gmail.com>
2022-08-28 23:43:56 +0200

    Added console log to illustrate problem better

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Foo from 'Foo'
 
 const root = createRoot(document.getElementById('root'));
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,33 @@
-import babel from 'vite-plugin-babel'
-import { defineConfig } from 'vite'
+import babel from "vite-plugin-babel";
+import { defineConfig } from "vite";
+
+const babelPlugin = babel({
+  babelConfig: {
+    presets: [
+      [
+        "@babel/preset-react",
+        {
+          runtime: "automatic",
+        },
+      ],
+    ],
+  },
+});
+
+const transform = babelPlugin.transform;
+
+babelPlugin.transform = (code, id, options) => {
+  // When running `vite`, "./src/Foo.js" is not printed here.
+  // It is printed when running `vite build`.
+  console.log(id);
+  return transform.call(babelPlugin, code, id, options);
+};
 
 export default defineConfig({
   resolve: {
     alias: {
-      Foo: './src/Foo.js',
+      Foo: "./src/Foo.js",
     },
   },
-  plugins: [
-    babel({
-      babelConfig: {
-        presets: [
-          [
-            '@babel/preset-react',
-            {
-              runtime: 'automatic',
-            },
-          ],
-        ],
-      },
-    }),
-  ],
-})
+  plugins: [babelPlugin],
+});
